cc65200c802a7148b3eef8a1a41d6a73991d7fc5
RicardoSalas17 <ricardosalas.1716@gmail.com>
2020-04-30 13:07:59 -0500

    carpetas de back y front creadas con los servicios de auth

---
+++ frontend/src/services/index.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+let baseURL;
+
+process.env.NODE_ENV === 'production'
+  ? (baseURL = 'here should be your production endpoint')
+  : (baseURL = 'http://localhost:3000');
+
+const service = axios.create({ withCredentials: true, baseURL });
+
+const MY_SERVICE = {
+  test: async () => {
+    return await service.get('/');
+  },
+  signup: async (user) => {
+    return await service.post('/signup', user);
+  },
+  login: async (user) => {
+    return await service.post('/login', user);
+  },
+  logOut: async () => {
+    return await service.get('/logout');
+  }
+};
+
+export default MY_SERVICE;
